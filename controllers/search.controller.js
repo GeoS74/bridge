c16f3ea22e8132179ae3aeb83ef84985c4f7e6c1
GeoS74 <gsirotkin@yandex.ru>
2022-11-02 19:40:05 +0500

    fixed searching position

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,32 +1,29 @@
 const db = require('../libs/db');
 const logger = require('../libs/logger');
-const { parserEng, parserRus, parserGlue } = require('../libs/article.parser');
+const { parserRus, parserGlue } = require('../libs/article.parser');
 
 module.exports.search = async (ctx) => {
   try {
     const start = Date.now();
-    ctx.status = 200;
 
-    let maxRank = 0;
-    let arr = await _getPositions(ctx.query.query);
-    arr = arr.filter((v, i) => {
-      if (i === 0) {
-        maxRank = v.rank * 1000;
-        return true;
-      }
-      return (v.rank * 1000 * 100 / maxRank) > 79.9;
-    });
+    let responseFullText = await _fullTextSearch(ctx.query.query);
+    responseFullText = _filterResponsesByRank(responseFullText);
 
+    if (!responseFullText.length || responseFullText[0].rank < 0.045) {
+      const glueTextSearch = await Promise.any(_makeRequestPool(ctx.query.query))
+        .catch(() => []);
 
-    if (!arr.length || arr[0].rank < 0.04) {
-      const pr = await Promise.any(makePromise(ctx.query.query))
-        .catch(_ => []);
-      arr = pr.concat(arr);
-
+      responseFullText = glueTextSearch.concat(responseFullText);
     }
-    // logger.info('query time: ', (Date.now() - start) / 1000);
+
+    responseFullText = _cleanDublicatePosition(responseFullText);
+    if (!responseFullText.length) {
+      throw new Error(`positions "${ctx.query.query}" not found`);
+    }
+
+    ctx.status = 200;
     ctx.body = {
-      positions: arr,
+      positions: responseFullText,
       time: (Date.now() - start) / 1000,
     };
   } catch (error) {
@@ -35,15 +32,101 @@ module.exports.search = async (ctx) => {
   }
 };
 
-function makePromise(query) {
+function _filterResponsesByRank(response) {
+  let maxRank = 0;
+  return response.filter((e, i) => {
+    if (i === 0) {
+      maxRank = e.rank * 1000;
+      return true;
+    }
+    return ((e.rank * 1000 * 100) / maxRank) > 79.9;
+  });
+}
+
+function _cleanDublicatePosition(response) {
+  const result = [];
+  response.forEach((v) => {
+    const dublicate = result.find((elem) => elem.brand_id === v.brand_id
+        && elem.provider_id === v.provider_id
+        && elem.eng_article_parse === v.eng_article_parse);
+
+    if (!dublicate) {
+      result.push(v);
+    }
+  });
+  return result;
+}
+
+function _getGlueStringForLike(str) {
+  return `%${parserGlue(str)}%`;
+}
+
+function _glueTextSearch(text) {
+  return new Promise((resolve, reject) => {
+    db.query(`
+      select
+        P.brand_id,
+        P.provider_id,
+        P.eng_article_parse,
+        P.article,
+        P.title,
+        M.price,
+        B.amount as amount_bovid,
+        P.amount as amount,
+        B.code, 
+        R.title as brand
+      from positions P
+      join prices M
+        on P.id=M.position_id
+      left join bovid B
+        on B.id=P.bovid_id
+      join brands R
+        on P.brand_id=R.id
+      where glue_article_parse like $1 AND
+        M.createdat = (
+          select max(createdat) from prices p3
+          where M.position_id=p3.position_id
+        ) 
+      order by article desc
+      limit 10
+  `, [_getGlueStringForLike(text)])
+      .then((res) => {
+        if (res.rows.length) {
+          resolve(res.rows);
+          return;
+        }
+        reject();
+      })
+      .catch((error) => {
+        // ошибка проброшенная от сюда с помощью throw ломает приложение
+        // чтобы не потерять ошибки запросов к БД они логируются здесь
+        logger.error(error.message);
+        reject();
+      });
+  });
+}
+
+function _makeRequestPool(query) {
   const arr = [];
-  for (let i = 0; i < query.length - 6; i++) {
-    const str = `%${parserGlue(query.substring(0, query.length - i))}%`;
-    arr.push(new Promise((resolve, reject) => {
-      db.query(`
+  for (let i = 0; i < query.length - 6; i += 1) {
+    arr.push(_glueTextSearch(query.substring(0, query.length - i)));
+  }
+  return arr;
+}
+
+function normalize(word) {
+  return word.replaceAll(' ', ' | ');
+}
+
+function _fullTextSearch(text) {
+  return db.query(`
     select
+      P.brand_id,
+      P.provider_id,
+      P.eng_article_parse,
       P.article,
       P.title,
+      ts_rank(to_tsvector(rus_article_parse), to_tsquery($1)) as rank,
       M.price,
       B.amount as amount_bovid,
       P.amount as amount,
@@ -56,117 +139,13 @@ function makePromise(query) {
       on B.id=P.bovid_id
     join brands R
       on P.brand_id=R.id
-    where glue_article_parse like $1 AND
+    where to_tsvector(rus_article_parse) @@ to_tsquery($1) AND
       M.createdat = (
         select max(createdat) from prices p3
         where M.position_id=p3.position_id
       ) 
-    order by article desc
-    limit 5
-    `, [str]).then((res) => {
-        if (res.rows.length) {
-          return resolve(res.rows)
-        }
-        reject(new Error())
-      })
-    }));
-  }
-  return arr;
-}
-
-function normalize(word) {
-  return word.replaceAll(' ', ' | ');
-}
-
-function _getPositionsStep2(text) {
-  return db.query(`
-  select 
-    P.article,
-    P.title,
-    ts_rank(to_tsvector(P.eng_article_parse), to_tsquery($1)) as rank,
-    M.price,
-    B.amount as amount_bovid,
-    P.amount as amount,
-    B.code, 
-    R.title as brand
-  from positions P
-  join prices M
-    on P.id=M.position_id
-  left join bovid B
-    on B.id=P.bovid_id
-  join brands R
-    on P.brand_id=R.id
-  where to_tsvector(P.eng_article_parse) @@ to_tsquery($1) AND
-    M.createdat = (
-      select max(createdat) from prices p3
-      where M.position_id=p3.position_id
-    ) 
-  ORDER BY ts_rank(to_tsvector(P.eng_article_parse), to_tsquery($1)) DESC
-  limit 10
-  `, [normalize(parserEng(text))])
-    .then((res) => res.rows);
-}
-
-function _getPositions(text) {
-  return db.query(`
-  select 
-    P.article,
-    P.title,
-    ts_rank(to_tsvector(rus_article_parse), to_tsquery($1)) as rank,
-    M.price,
-    B.amount as amount_bovid,
-    P.amount as amount,
-    B.code, 
-    R.title as brand
-  from positions P
-  join prices M
-    on P.id=M.position_id
-  left join bovid B
-    on B.id=P.bovid_id
-  join brands R
-    on P.brand_id=R.id
-  where to_tsvector(rus_article_parse) @@ to_tsquery($1) AND
-    M.createdat = (
-      select max(createdat) from prices p3
-      where M.position_id=p3.position_id
-    ) 
-  ORDER BY ts_rank(to_tsvector(rus_article_parse), to_tsquery($1)) DESC
-  limit 10
+    ORDER BY ts_rank(to_tsvector(rus_article_parse), to_tsquery($1)) DESC
+    limit 10
   `, [normalize(parserRus(text))])
-    .then((res) => res.rows);
-}
-
-function _getPositions__(text) {
-  return db.query(`
-  select 
-    p1.bovid_id, 
-    R.title as brand, 
-    V.title as provider, 
-    B.code, 
-    B.article as b_article, 
-    p1.article, 
-    p1.title,
-    B.amount as ut_amount, 
-    p1.amount as pos_amount, 
-    M.price,
-    M.createdat
-  from positions p1
-  join prices M
-    on p1.id=M.position_id
-  left join bovid B
-    on B.id=p1.bovid_id
-  join brands R
-    on p1.brand_id=R.id
-  join providers V
-    on p1.provider_id=V.id
-  where M.createdat = (
-    select max(createdat) from prices p3
-    where M.position_id=p3.position_id
-  ) 
-    and p1.bovid_id is not null 
-    and B.amount != 0
-    and p1.brand_id=1 and provider_id=1
-  offset 0 limit 10;
-  `)
     .then((res) => res.rows);
 }
