47a563e7940cb5d33fcddec9a2d4a4b1a6303c86
GeoS74 <gsirotkin@yandex.ru>
2023-01-30 15:26:06 +0500

    added README.md

--- middleware/validators/position.params.validator.js
+++ middleware/validators/position.params.validator.js
@@ -1,12 +1,18 @@
+const fs = require('fs');
+
+const logger = require('../../libs/logger');
+
 module.exports = async (ctx, next) => {
   const brandId = parseInt(ctx.request?.body?.brandId, 10);
   if (!brandId) {
+    _deleteFile(ctx.request.files);
     ctx.throw(400, 'brand not found');
   }
   ctx.request.body.brandId = brandId;
 
   const providerId = parseInt(ctx.request?.body?.providerId, 10);
   if (!providerId) {
+    _deleteFile(ctx.request.files);
     ctx.throw(400, 'provider not found');
   }
   ctx.request.body.providerId = providerId;
@@ -15,3 +21,16 @@ module.exports = async (ctx, next) => {
 
   await next();
 };
+
+function _deleteFile(files) {
+  for (const file of Object.values(files)) {
+    // received more than 1 file in any field with the same name
+    if (Array.isArray(file)) {
+      _deleteFile(file);
+    } else {
+      fs.unlink(file.filepath, (err) => {
+        if (err) logger.error(err);
+      });
+    }
+  }
+}
