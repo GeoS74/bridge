9a3779cf74b1e50740c37569c84e85c813e517d8
GeoS74 <gsirotkin@yandex.ru>
2023-02-12 16:00:02 +0300

    linter

--- middleware/access.check.js
+++ middleware/access.check.js
@@ -10,15 +10,8 @@ module.exports = async (ctx, next) => {
 
   try {
     const token = ctx.get('Authorization').split(' ')[1];
-
-    console.log(process.env.JWT_SECRET_KEY)
-  console.log(config.jwt.secretKey)
-  console.log(jwt.verify(token, config.jwt.secretKey))
-
-
     ctx.user = _jwtDecode(token).user;
   } catch (error) {
-    console.log(error.message)
     ctx.set('WWW-Authenticate', 'Bearer');
     ctx.throw(401, 'invalid access token');
   }
