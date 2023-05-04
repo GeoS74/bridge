24da153717fd2e07446a4c91ef20a2122545a0bb
GeoS74 <gsirotkin@yandex.ru>
2023-05-04 18:48:19 +0300

    fixed timer

--- routes/external.api.routes.js
+++ routes/external.api.routes.js
@@ -1,8 +1,9 @@
 const Router = require('koa-router');
 
 const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
 const position = require('../controllers/external.api.controller');
-// const accessCheck = require('../middleware/access.check');
+const accessCheck = require('../middleware/access.check');
 const { timer } = require('../libs/myfunc');
 const config = require('../config');
 
@@ -10,6 +11,7 @@ const router = new Router({ prefix: '/api/bridge/external' });
 
 router.get(
   '/voshod',
+  accessCheck,
   position.countPages,
   // position.readerV1,
   position.readerV2,
@@ -17,6 +19,12 @@ router.get(
 
 module.exports = router.routes();
 
-timer(['07:00:00', '10:00:00', '13:00:00', '16:00:00'], () => {
-  fetch(`http://${config.server.host}:${config.server.port}/api/bridge/external/voshod`);
+timer(['18:44:00', '10:00:00', '13:00:00', '16:00:00'], () => {
+  const token = jwt.sign('say hi', config.jwt.secretKey);
+  // этот fetch упадёт если к роуту '/voshod' подключить accessCheck и не передать токен
+  fetch(`http://${config.server.host}:${config.server.port}/api/bridge/external/voshod`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 });
