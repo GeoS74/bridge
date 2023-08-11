812b7f650c60c1af824adedd34547dcf1678dc11
geos <gsirotkin@yandex.ru>
2023-08-11 10:43:24 +0500

    ...

--- libs/db.js
+++ libs/db.js
@@ -28,27 +28,4 @@ const pool = new Pool({
 });
 
 module.exports.query = (text, params) => pool.query(text, params);
-module.exports.p = new Pool({
-  user: config.postgres.user,
-  host: config.postgres.host,
-  database: config.postgres.database,
-  password: config.postgres.password,
-  port: config.postgres.port,
-
-  // idleTimeoutMillis - количество миллисекунд,
-  // в течение которых клиент должен бездействовать в пуле и не быть проверенным
-  // прежде чем он будет отключен от бэкенда и удален
-  // по умолчанию 10000 (10 секунд) — установите 0,
-  // чтобы отключить автоотключение неработающих клиентов
-  idleTimeoutMillis: 10000,
-
-  // connectionTimeoutMillis - количество миллисекунд ожидания
-  // до истечения времени ожидания при подключении нового клиента
-  // по умолчанию это 0, что означает отсутствие тайм-аута
-  connectionTimeoutMillis: 10000,
-
-  // max - максимальное количество клиентов,
-  // которое должно содержаться в пуле
-  // по умолчанию установлено значение 10.
-  max: 50,
-});
+module.exports.p = () => pool;
