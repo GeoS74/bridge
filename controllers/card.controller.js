017dfed5bba51d732c90411e7bf2ab20f76fd2b9
GeoS74 <gsirotkin@yandex.ru>
2023-10-21 16:43:23 +0300

    v.0.0.5

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -177,70 +177,54 @@ module.exports.getCards = async (ctx) => {
   ctx.body = cards.map((card) => mapper(card));
 };
 
-// функция возвращает минимизированный результат для построения sitemap
 async function _getAllCards(limit, offset) {
   return db.query(`
     select
       P.id,
       P.createdat,
-      P.alias
+      P.brand_id,
+      R.title as brand_title,
+      P.provider_id,
+      CONCAT('Склад № ', P.provider_id) as stock,
+      P.article,
+      P.title,
+      P.photo,
+      P.alias,
+      case
+        when M.createdat < NOW() - interval '${config.search.ttl}' then 0
+        else (M.price*(1+M.profit/100)) end
+        as settlement_price,
+      B.amount as amount_bovid,
+      case
+        when M.createdat < NOW() - interval '${config.search.ttl}' then 0
+        else P.amount end
+        as amount,
+      B.code,
+      B.uid,
+      P.manufacturer,
+      B.storage,
+      B.weight,
+      B.width,
+      B.length,
+      B.storage
     from positions P
+    join prices M
+      on P.id=M.position_id
+    left join bovid B
+      on B.id=P.bovid_id
+    join brands R
+      on P.brand_id=R.id
+    join providers V
+      on P.provider_id=V.id
+    where M.createdat = (
+        select max(createdat) from prices p3
+        where M.position_id=p3.position_id
+      )
     ORDER BY P.id DESC
     LIMIT $1 OFFSET $2
   `, [limit, offset])
     .then((res) => res.rows);
 }
-
-// такой запрос к БД забирает слишком много ресурсов
-//
-// async function _getAllCards(limit, offset) {
-//   return db.query(`
-//     select
-//       P.id,
-//       P.createdat,
-//       P.brand_id,
-//       R.title as brand_title,
-//       P.provider_id,
-//       CONCAT('Склад № ', P.provider_id) as stock,
-//       P.article,
-//       P.title,
-//       P.photo,
-//       P.alias,
-//       case
-//         when M.createdat < NOW() - interval '${config.search.ttl}' then 0
-//         else (M.price*(1+M.profit/100)) end
-//         as settlement_price,
-//       B.amount as amount_bovid,
-//       case
-//         when M.createdat < NOW() - interval '${config.search.ttl}' then 0
-//         else P.amount end
-//         as amount,
-//       B.code,
-//       B.uid,
-//       P.manufacturer,
-//       B.storage,
-//       B.weight,
-//       B.width,
-//       B.length,
-//       B.storage
-//     from positions P
-//     join prices M
-//       on P.id=M.position_id
-//     left join bovid B
-//       on B.id=P.bovid_id
-//     join brands R
-//       on P.brand_id=R.id
-//     join providers V
-//       on P.provider_id=V.id
-//     where M.createdat = (
-//         select max(createdat) from prices p3
-//         where M.position_id=p3.position_id
-//       )
-//     ORDER BY P.id DESC
-//     LIMIT $1 OFFSET $2
-//   `, [limit, offset])
-//     .then((res) => res.rows);
-// }
 
 module.exports.getCountCards = async (ctx) => {
   const count = await _getCountCards();
--- test/card.test.js
+++ test/card.test.js
@@ -90,13 +90,11 @@ describe('/test/card.test.js', () => {
     it('test limit', async () => {
       const response = await fetch(`http://localhost:${config.server.port}/api/bridge/card/?limit=1`)
         .then(_getData);
-        console.log(response.data)
       expect(response.status, 'сервер возвращает статус 200').to.be.equal(200);
       expect(response.data, 'сервер возвращает массив').that.is.an('array');
       expect(response.data.length, 'сервер возвращает массив из 1-го элемента').to.be.equal(1);
       _expectFieldState.call(this, response.data[0]);
-      // API возвращает упрощённый запрос к БД (артикул позиции отсутствует)
-      // expect(response.data[0].article, 'сервер возвращает последний добавленный элемент').to.be.equal('4320X-1201010-01');
+      expect(response.data[0].article, 'сервер возвращает последний добавленный элемент').to.be.equal('4320X-1201010-01');
     });
 
     it('test offset', async () => {
@@ -106,8 +104,7 @@ describe('/test/card.test.js', () => {
       expect(response.data, 'сервер возвращает массив').that.is.an('array');
       expect(response.data.length, 'сервер возвращает массив из 1-го элемента').to.be.equal(1);
       _expectFieldState.call(this, response.data[0]);
-      // API возвращает упрощённый запрос к БД (артикул позиции отсутствует)
-      // expect(response.data[0].article, 'сервер возвращает следующий элемент').to.be.equal('8608014*01 5557');
+      expect(response.data[0].article, 'сервер возвращает следующий элемент').to.be.equal('8608014*01 5557');
 
       response = await fetch(`http://localhost:${config.server.port}/api/bridge/card/?limit=1&offset=10`)
         .then(_getData);
